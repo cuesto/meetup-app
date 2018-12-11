8e7f91810667e3cc9140c867d3bb2d797a53f238
Jhonatan Cuesto <jhonatan_cuesto@claro.com.do>
2018-12-11 11:51:41 -0400

    lesson 16

--- src/main.js
+++ src/main.js
@@ -4,12 +4,24 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import DateFilter from "./filters/date";
+import * as firebase from "firebase";
+import AlertCmp from "./components/Shared/Alert.vue";
 
 Vue.config.productionTip = false;
 Vue.filter("date", DateFilter);
+Vue.component("app-alert", AlertCmp);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCqCQUOg2YDQJPp_T73VSMg8G43Flfl4I0",
+      authDomain: "meetup-app-2c038.firebaseapp.com",
+      databaseURL: "https://meetup-app-2c038.firebaseio.com",
+      projectId: "meetup-app-2c038",
+      storageBucket: "meetup-app-2c038.appspot.com"
+    });
+  }
 }).$mount("#app");
--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import * as firebase from "firebase";
 
 Vue.use(Vuex);
 
@@ -25,14 +26,25 @@ export default new Vuex.Store({
         description: "It's Paris!"
       }
     ],
-    user: {
-      id: "ajaldslfalsk12",
-      registeredMeetups: ["aadsfhbkhlk1241"]
-    }
+    user: null,
+    loading: false,
+    error: null
   },
   mutations: {
     createMeetup(state, payload) {
       state.loadedMeetups.push(payload);
+    },
+    setUser(state, payload) {
+      state.user = payload;
+    },
+    setLoading(state, payload) {
+      state.loading = payload;
+    },
+    setError(state, payload) {
+      state.error = payload;
+    },
+    clearError(state) {
+      state.error = null;
     }
   },
   actions: {
@@ -46,6 +58,49 @@ export default new Vuex.Store({
         id: "ksdfksdlmsisdflj3"
       };
       commit("createMeetup", meetup);
+    },
+    signUserUp({ commit }, payload) {
+      commit("setLoading", true);
+      commit("clearError");
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(payload.email, payload.password)
+        .then(user => {
+          commit("setLoading", false);
+          const newUser = {
+            id: user.uid,
+            registeredMeetups: []
+          };
+          commit("setUser", newUser);
+        })
+        .catch(error => {
+          commit("setLoading", false);
+          commit("setError", error);
+          console.log(error);
+        });
+    },
+    signUserIn({ commit }, payload) {
+      commit("setLoading", true);
+      commit("clearError");
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(payload.email, payload.password)
+        .then(user => {
+          commit("setLoading", false);
+          const newUser = {
+            id: user.uid,
+            registeredMeetups: []
+          };
+          commit("setUser", newUser);
+        })
+        .catch(error => {
+          commit("setLoading", false);
+          commit("setError", error);
+          console.log(error);
+        });
+    },
+    clearError({ commit }) {
+      commit("clearError");
     }
   },
   getters: {
@@ -63,6 +118,15 @@ export default new Vuex.Store({
           return meetup.id === meetupId;
         });
       };
+    },
+    user(state) {
+      return state.user;
+    },
+    loading(state) {
+      return state.loading;
+    },
+    error(state) {
+      return state.error;
     }
   }
 });
